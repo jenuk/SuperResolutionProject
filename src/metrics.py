56d27241f9d9fb1733c2e2cae03d72e8525e5b0d
jenuk <gandalf.d@web.de>
2020-06-29 12:12:26 +0200

    added validation

--- src/metrics.py
+++ src/metrics.py
@@ -17,4 +17,28 @@ def mse_loss(out, targets):
         shape `()`
     """
 
-    return torch.mean(torch.sum((out - targets)**2, axis=(1,2,3)), axis=0)
+    return torch.mean((out - targets)**2, axis=(0,1,2,3))
+
+def psnr(out, targets):
+    """
+    return peak signal-to-noise ratio between `out` and `targets`.
+
+    Parameters
+    ----------
+    out : torch.tensor
+        of shape `(batch_size, channel, h, w)`.
+    target : torch.tensor
+        of shape `(batch_size, channel, h, w)`.
+
+    Returns
+    -------
+    torch.tensor
+        shape `(batch_size)`
+    """
+
+    MSEs = torch.mean((out - targets)**2, axis=(1,2,3))
+    maxs = targets
+    while len(maxs.shape) > 1:
+        maxs = torch.max(maxs, axis=1).values
+
+    return 10*(2*torch.log(maxs) - torch.log(MSEs))
--- src/utils.py
+++ src/utils.py
@@ -24,3 +24,22 @@ def inner_pad(x, factor, padding = 0):
     res[..., ::factor, ::factor] = x
 
     return res
+
+def clip(x):
+    """
+    Clips values of x between 0 and 1.
+
+    Parameters
+    ----------
+    x : torch.tensor
+        any shape
+
+    Returns
+    -------
+    torch.tensor
+        same shape as x
+    """
+
+    out = torch.max(x, torch.tensor(0.0))
+    out = torch.min(out, torch.tensor(1.0))
+    return out
